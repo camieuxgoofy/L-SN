7b657547c0b5fa0e084366394d9b7f4c3e62ffea
Camyoumu <72339948+camieuxgoofy@users.noreply.github.com>
2023-07-01 22:56:38 +0700

    Create coad.ts

---
+++ L-JS/React/coad.ts
@@ -0,0 +1,287 @@
+before::
+function proclaim(status?: string, repeat?: number) {
+  for (let i = 0; i < repeat || 0; i += 1) {
+    console.log(`I'm ${status || 'not ready...'}`);
+  }
+}
+
+proclaim();
+proclaim('ready?');
+proclaim('ready!', 3);
+
+after::
+function proclaim(status = 'not ready...', repeat = 1) {
+  for (let i = 0; i < repeat; i += 1) {
+    console.log(`I'm ${status}`);
+  }
+}
+
+proclaim();
+proclaim('ready?');
+proclaim('ready!', 3);
+
+done::
+let customersArray = ['Custy Stomer', 'C. Oostomar', 'C.U.S. Tomer', 3432434, 'Custo Mer', 'Custopher Ustomer', 3432435, 'Kasti Yastimeur'];
+
+//Write Your Code here:
+function checkCustomersArray() {
+  for (let i = 0; i < customersArray.length; i++) {
+    if (typeof customersArray[i] !== 'string') {
+      console.log(`Type error: ${customersArray[i]} should be a string!`);
+    }
+  }
+}
+
+function stringPush(val) {
+  if (typeof val === 'string') {
+    customersArray.push(val);
+  }
+}
+
+checkCustomersArray();
+
+
+before::
+let petOnSale = 'chinchilla';
+let ordersArray = [
+  ['rat', 2], 
+  ['chinchilla', 1], 
+  ['hamster', 2], 
+  ['chinchilla', 50]
+];
+
+// Write your code below:
+
+
+
+after::
+enum Pet {
+  Hamster = 'HAMSTER',
+  Rat = 'RAT',
+  Chinchilla = 'CHINCHILLA',
+  Tarantula = 'TARANTULA'
+}
+
+let petOnSaleTS: Pet = Pet.Chinchilla;
+
+let ordersArrayTS: [Pet, number][] = [
+  [Pet.Rat, 2],
+  [Pet.Chinchilla, 1],
+  [Pet.Hamster, 2],
+  [Pet.Chinchilla, 50]
+];
+
+ordersArrayTS.push([Pet.Hamster, 1]);
+
+
+done::
+type Human = {name: string, job: string};
+type Dog = {name: string, tailWagSpeed: number};
+
+type Family<T> = {
+  parents: [T, T], mate: T, children: T[]
+};
+//Do not change the code above this line.
+
+//Provide type annotations for the variables below:
+let theFamily: Family<number> = {
+  parents: [3, 4], mate: 9, children: [5, 30, 121]
+};
+
+let someFamily: Family<boolean> = {
+  parents: [true, true], mate: false, 
+  children: [false, false, true, true]
+};
+
+let aFamily: Family<Human> = {
+  parents: [
+    {name: 'Mom', job: 'software engineer'},
+    {name: 'Dad', job: 'coding engineer'}
+  ],
+  mate: {name: 'Matesky', job: 'engineering coder'},
+  children: [{name: 'Babesky', job: 'none'}]
+};
+
+let anotherFamily: Family<Dog> = {
+  parents: [
+    {name: 'Momo', tailWagSpeed: 3},
+    {name: 'Dado', tailWagSpeed: 100}
+  ],
+  mate: {name: 'Cheems', tailWagSpeed: 7},
+  children: [
+    {name: 'Puppin', tailWagSpeed: 0.001},
+    {name: 'Puppenaut', tailWagSpeed: 0.0001},
+    {name: 'Puppenator', tailWagSpeed: 0.01}
+  ]
+};
+
+done::
+function getFilledArray<T>(value: T, n: number): T[] {
+  return Array(n).fill(value);
+}
+
+let stringArray: string[];
+let numberArray: number[];
+let personArray: {name: string, age: number}[];
+let coordinateArray: [number, number][];
+
+// Assign values to the variables using getFilledArray<T>()
+stringArray = getFilledArray<string>('hi', 6);
+numberArray = getFilledArray<number>(9, 6);
+personArray = getFilledArray<{name: string, age: number}>({name: 'J. Dean', age: 24}, 6);
+coordinateArray = getFilledArray<[number, number]>([3, 4], 6);
+
+done::
+function formatValue(value: string | number) {
+  if (typeof value === 'string') {
+    console.log(value.toLowerCase());
+  } else if (typeof value === 'number') {
+    console.log(value.toFixed(2));
+  }
+}
+
+formatValue('Hiya');
+formatValue(42);
+
+
+done::
+type Cat = {
+  name: string;
+  run: () => string;
+}
+
+type Fish = {
+  name: string;
+  swim: () => string;
+}
+
+const siameseCat = { 
+  name: 'Proxie', 
+  run: () => 'pitter pat'
+}
+
+const bettaFish = { 
+  name: 'Neptune', 
+  swim: () => 'bubble blub'
+}
+
+function move(pet: Cat | Fish) {
+  if ('run' in pet) {
+    return pet.run();
+  }
+  if ('swim' in pet) {
+    return pet.swim();
+  }
+}
+
+console.log(move(siameseCat))
+
+
+done::
+type Pasta = {
+  menuName: string;
+  boil: () => string;
+}
+
+type Meat = {
+  menuName: string;
+  panFry: () => string;
+}
+
+const fettuccine = {
+  menuName: 'Fettuccine',
+  boil: () => 'Heat water to 212 degrees',
+}
+
+const steak = {
+  menuName: 'New York Strip Steak',
+  panFry: () => 'Heat oil to 350 degrees',
+}
+
+function prepareEntree(entree: Pasta | Meat) {
+  if ('boil' in entree) {
+    return entree.boil();
+  } else {
+    return entree.panFry();
+  }
+}
+
+console.log(prepareEntree(fettuccine));
+
+done::
+type Metal = {
+  magnetize: () => string;
+}
+
+type Glass = {
+  melt: () => string;
+}
+
+const iron = {
+  magnetize: () => 'Electromagnet activated'
+}
+
+const bottle = {
+  melt: () => 'Furnace set to 2,700 degrees'
+}
+
+
+function recycle(trash: Metal | Glass) {
+  // Add your code below:
+   if ('magnetize' in trash) {
+    return trash.magnetize();
+  }
+  return trash.melt();
+}
+
+console.log(recycle(iron));
+
+done::
+// Write an interface here
+interface Run {
+  miles: number;
+}
+
+function updateRunGoal(run: Run) {
+  console.log(`
+Miles left:       ${50 - run.miles}
+Percent of goal:  ${(run.miles / 50) * 100}% complete
+  `)
+}
+
+updateRunGoal({
+  miles: 5,
+})
+
+
+done::
+interface Directory {
+  addFile: (name: string) => void;
+  config: {
+    default: {
+      encoding: string;
+      permissions: string;
+    };
+  };
+}
+
+class DesktopDirectory implements Directory {
+  config = {
+    default: {
+      encoding: 'utf-8',
+      permissions: 'drw-rw-rw-',
+    }
+  }
+
+  addFile(name: string) {
+    console.log(`Adding file: ${name}`);
+  }
+
+  showPreview(name: string) {
+    console.log(`Opening preview of file: ${name}`);
+  }
+}
+
+const Desktop = new DesktopDirectory();
+
+console.log(Desktop.config);
